99549bcab914e1ee882defdf4448980f96ea885a
andrea <fior_di_maso@libero.it>
2020-11-10 11:24:45 +0100

    set 1h timer

--- BSR_tracking.py
+++ BSR_tracking.py
@@ -9,7 +9,7 @@ class BestSellerRankTracker:
         self.br = browser
         self.urls = product_urls
     
-    def get_BSR(self):
+    def get_BSR(self, delay=0):
         for product, url in self.urls.items():
 
             JSON_file_path = f"{product}.json"
@@ -20,14 +20,13 @@ class BestSellerRankTracker:
                     data = json.load(f)
             except FileNotFoundError:
                 data = dict()
-                data[actual_date] = []
-            
-            # open product page
-            self.br.get(url)
-
+                
             # retrieve the text containing the BSR
             while True:    
                 try:
+                    # open product page
+                    self.br.get(url)
+                    time.sleep(delay)
                     text = self.br.find_element_by_xpath('//*[@id="productDetails_detailBullets_sections1"]/tbody/tr[3]/td/span/span[1]').text
                     break
                 except Exception as e:
@@ -37,6 +36,10 @@ class BestSellerRankTracker:
             # add the BSR to the json file
             BSR = int(text.replace(',', '').split()[1])
             print(f"BSR of {product} is {BSR}")
+            
+            if actual_date not in data:
+                data[actual_date] = []
+
             data[actual_date].append({"Time": datetime.now().strftime("%H:%M:%S"), "BSR": BSR})
 
             with open(JSON_file_path, 'w') as f:
@@ -51,15 +54,15 @@ chrome_options.add_argument("--headless")
 
 BROWSER = webdriver.Chrome('C:/Users/gedeo/OneDrive/Documenti/Python/chromedriver.exe', options=chrome_options)
 PRODUCT_URLS = {
+                # "PM Compressore Portatile": "https://www.amazon.it/gp/product/B08B68GG1W/",
                 "BidMamba": 'https://www.amazon.it/gp/product/B08F7QCT8M', 
                 "Smart Safe": 'https://www.amazon.it/gp/product/B08LL7P37P/',
-                "PM Compressore Portatile": "https://www.amazon.it/gp/product/B08B68GG1W/",
                 }
 
 tracker = BestSellerRankTracker(BROWSER, PRODUCT_URLS)
 while True:
     tracker.get_BSR()
-    time.sleep(60)
+    time.sleep(3600)
 
 # close Chrome window
 tracker.br.quit()
